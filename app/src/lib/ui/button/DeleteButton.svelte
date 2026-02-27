0ecf595a025ee13795e6656b0407ea2a3db14a09
Karim Ben Amara <karimbenamara91@gmail.com>
2026-02-27 23:57:38 +0100

    added delete button

---
+++ app/src/lib/ui/button/DeleteButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import AsyncButton from './AsyncButton.svelte';
+
+	interface Props {
+		onClick: () => Promise<any>;
+	}
+
+	async function handleDelete() {}
+</script>
+
+<AsyncButton onclick={async () => await handleDelete()}>
+	{#snippet idleView()}
+		<div>Delete</div>
+	{/snippet}
+
+	{#snippet endView()}
+		<div>Deleted!</div>
+	{/snippet}
+</AsyncButton>
