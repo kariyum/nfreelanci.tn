9b513a8bb613b7935bf3eb6a80a2cc8f5126e9d5
Karim Ben Amara <karimbenamara91@gmail.com>
2026-03-18 22:40:17 +0100

    add link to /login in the unauthorized message

--- app/src/lib/pages/Error.svelte
+++ app/src/lib/pages/Error.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { invalidate } from '$app/navigation';
+	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 
@@ -15,7 +16,9 @@
 </script>
 
 {#if status === 401}
-	<div>🖐️ You are unauthorized to view this resource, please login...</div>
+	<div>
+		🖐️ You are unauthorized to view this resource, please <a href={resolve('/login')}>login..</a>.
+	</div>
 {:else if message && message.trim().length > 0}
 	<div>
 		{status}
